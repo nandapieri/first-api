7a3ca6b73b0e813f8bc330c1a77b21c65985a480
Fernanda Pieri <nandapieri@gmail.com>
2021-03-22 01:17:09 -0300

    enviando email de confirmacao

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -9,12 +9,12 @@ router.get('/',async (req,res) => {
     const c = await Cadastro.find();
     res.json(c);
   } catch(err) {
-    res.json({ message: err });
+    res.status(404).json({ message: err });
   }
-  res.send('get do cadastro')
+  res.status(200).send('get do cadastro')
 })
 
-//faz um cadastro 
+//faz um cadastro
 router.post('/', async (req,res) => {
   const c = new Cadastro({
     nomeCompleto: req.body.nomeCompleto,
@@ -26,11 +26,40 @@ router.post('/', async (req,res) => {
   try {
     //salva no banco
     const saved = await c.save();
-    res.json(saved);
+    res.status(200).json(saved);
+    sendMail(req.body.email,req.body.nomeCompleto);
   } catch(err) {
-    res.json({ message: err });
+    res.status(404).json({ message: err });
   }
 
+function sendMail(destMail,name) {
+  var nodemailer = require('nodemailer');
+  require('dotenv/config');
+
+  var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS
+    }
+  });
+
+  var mailOptions = {
+    from: process.env.EMAIL,
+    to: destMail,
+    subject: 'Bem vindo a Geek Week!',
+    text: 'Olá, '+name+'!\n'+'Você está recebendo esse email para confirmar seu cadastro na geek week!\n'+
+      'Estamos muito felizes em ter você conosco!\nEm breve enviaremos mais novidades!\n\n Geek Store Team'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  }
 
 });
 
