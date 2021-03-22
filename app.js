341f88e39291c70543cc969c1931f875eba13cd6
Fernanda Pieri <nandapieri@gmail.com>
2021-03-22 02:10:10 -0300

    deploy heroku - mudanca na porta

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 const mongoose = require('mongoose');
 require('dotenv/config');
 const bodyParser = require('body-parser');
