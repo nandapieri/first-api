427c36f618af11d30f71b5810a7f5bc7ad598748
Fernanda Pieri <nandapieri@gmail.com>
2021-03-26 11:32:06 -0300

    alteracao no readme

--- README.md
+++ README.md
@@ -7,3 +7,36 @@ O objetivo dessa aplicação realizar cadastro de pessoas via chatbot e enviar u
 
 Entre no chatbot (https://bityli.com/dr4GV) e selecione a opção "Junte-se a nós na Geek Week" (descendo o menu na seta para baixo). Cadastre-se e verifique a sua caixa de emails!
 O bot faz validação de CPF, gere um CPF válido em https://www.4devs.com.br/gerador_de_cpf
+
+## Instruções
+
+Para rodar esse projeto é necessário ter instalados o Node js e o MongoDB. Para esse projeto criei o banco de dados no mLab, que é gratuito.
+
+* Crie uma cópia deste repositório:
+```
+  $ git clone https://github.com/nandapieri/testeSmarkio
+```
+
+* Navegue até a pasta do projeto via terminal e baixe as dependências do projeto:
+```
+  $ npm install
+```
+
+* Crie um banco de dados na sua instancia do MongoDB e pegue a URI para se conectar com o mesmo.
+
+* Copie o arquivo .env.ex e crie um arquivo .env:
+```
+  $ cp .env.ex .env
+```
+
+* Abra o arquivo .env e adicione a URI do seu banco de dados MongoDB e o email e senha que irá utilizar para mandar o email de confirmação. Importante: é necessário ter uma conta no gmail e criar uma senha para aplicativo nele. https://support.google.com/accounts/answer/185833?hl=pt-BR
+
+
+* Para iniciar digite no terminal, ainda na pasta do projeto:
+```
+  $ npm start
+```
+
+* Você pode usar o Postman para testar o servidor.
+
+* Para parar o servidor, digite ctrl+C no terminal.
--- test/cadastro.test.js
+++ test/cadastro.test.js
@@ -6,14 +6,48 @@ var expect = chai.expect;
 var urlBase = "https://bot-api-fernanda.herokuapp.com";
 
 // Criamos nosso primeiro caso de teste e fornecemos uma descricao utilizando describe
-describe("Teste API",function(){
+describe("Teste API GET",function(){
   // a funcao it eh o que vamos testar realmente, neste caso o endpoint /cadastro
   it("Deve receber a lista de cadastros", function(done){
 
     request.get(
       {
-        //url : urlBase + "/cadastro"
-        url: "https://bot-api-fernanda.herokuapp.com/cadastro"
+        url : urlBase + "/cadastro"
+      },
+      function(error, response, body){
+
+        // precisamos converter o retorno para um objeto json
+        var _body = {};
+        try{
+          _body = JSON.parse(body);
+        }
+        catch(e){
+          _body = {};
+        }
+
+        // utilizando a funcao expect do chai, vamos verificar se o resultado da chamada foi sucesso (200)
+        expect(response.statusCode).to.equal(200);
+
+        done(); // avisamos o test runner que acabamos a validacao e ja pode proseeguir
+      }
+    );
+  });
+});
+
+
+describe("Teste API POST",function(){
+  // a funcao it eh o que vamos testar realmente, neste caso o endpoint /cadastro
+  it("Deve realizar um cadastro", function(done){
+
+    request.post(
+      {
+        url: "https://bot-api-fernanda.herokuapp.com/cadastro",
+        body: {
+            "nomeCompleto": "Fernanda P",
+            "email": "nandapieri@gmail.com",
+            "cpf": "52236748043",
+            "telefone": "31995566884"
+          }
       },
       function(error, response, body){
 
